c42b566aa3f476a9a31c62dff1095aab92f45816
jison06 <ison.84@buckeyemail.osu.edu>
2020-11-15 11:17:04 -0500

    Seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,32 @@ end
   Category.create(difficulty: difficulty, score: score)
 end
 
-difficulties = Category.all.map(&:id)
+easy = Category.find_by(difficulty: 'easy')
+Card.create(category_id: easy.id, workout: 'Pushups', description: 3)
+Card.create(category_id: easy.id, workout: 'Sit Ups', description: 10)
+Card.create(category_id: easy.id, workout: 'Burpees', description: 5)
+Card.create(category_id: easy.id, workout: 'Squats', description: 10)
+Card.create(category_id: easy.id, workout: 'Lunge', description: 5)
+Card.create(category_id: easy.id, workout: 'Tuck-Jumps', description: 3)
+Card.create(category_id: easy.id, workout: 'Leg Raises', description: 10)
+Card.create(category_id: easy.id, workout: 'Calf Raises', description: 5)
+Card.create(category_id: easy.id, workout: 'Shoulder Taps', description: 5)
+Card.create(category_id: easy.id, workout: 'Russian Twists', description: 10)
+Card.create(category_id: easy.id, workout: 'Squat-Reach-Jump', description: 5)
+Card.create(category_id: easy.id, workout: 'Dips', description: 3)
 
-60.times do
-  Card.create(category_id: difficulties.sample, workout: Faker::Games::Pokemon.name, description: Faker::Games::Pokemon.move)
-end
+medium = Category.find_by(difficulty: 'medium')
+Card.create(category_id: medium.id, workout: 'Pushups', description: 10)
+Card.create(category_id: medium.id, workout: 'Sit Ups', description: 20)
+Card.create(category_id: medium.id, workout: 'Burpees', description: 10)
+Card.create(category_id: medium.id, workout: 'Squats', description: 20)
+Card.create(category_id: medium.id, workout: 'Lunge', description: 15)
+Card.create(category_id: medium.id, workout: 'Tuck-Jumps', description: 10)
+
+hard = Category.find_by(difficulty: 'hard')
+Card.create(category_id: hard.id, workout: 'Pushups', description: 25)
+Card.create(category_id: hard.id, workout: 'Sit Ups', description: 50)
+Card.create(category_id: hard.id, workout: 'Burpees', description: 20)
+Card.create(category_id: hard.id, workout: 'Squats', description: 50)
+Card.create(category_id: hard.id, workout: 'Lunge', description: 30)
+Card.create(category_id: hard.id, workout: 'Tuck-Jumps', description: 25)
